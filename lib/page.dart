d79da90e4c387ad98d30967e33decf64c7a1242d
ubonwan215 <78993405+ubonwan215@users.noreply.github.com>
2021-03-20 11:44:29 +0700

    Initial commit

---
+++ lib/page.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:layout_example/screen/activity_screen.dart';
+import 'package:layout_example/screen/article_screen.dart';
+import 'package:layout_example/screen/home_screen.dart';
+
+class Page {
+  final String title;
+  final Icon icon;
+  final String label;
+  final Widget page;
+
+  Page({this.title, this.icon, this.label, this.page});
+}
+
+List<Page> pages = [
+  Page(
+    icon: Icon(Icons.home),
+    label: "home",
+    page: HomeScreen(),
+    title: "หน้าแรก",
+  ),
+  Page(
+    icon: Icon(Icons.home),
+    label: "news",
+    page: ArticleScreen(),
+    title: "ข่าวสาร",
+  ),
+  Page(
+    icon: Icon(Icons.home),
+    label: "activity",
+    page: ActivityScreen(),
+    title: "กิจกรรม",
+  ),
+];
---
+++ lib/screen/main_screen.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:layout_example/page.dart';
+
+class MainScreen extends StatefulWidget {
+  MainScreen({Key key}) : super(key: key);
+
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+
+  int currentIndex = 0;
+
+  void onIndexChanged(int index) {
+    setState(() {
+      currentIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            // UserAccountsDrawerHeader(
+            //   currentAccountPicture: CircleAvatar(
+            //     backgroundImage: NetworkImage(
+            //         "https://assets.brandinside.asia/uploads/2018/04/cat-cat-cat.jpg"),
+            //   ),
+            //   accountName: Text("Kaem Ubonwan"),
+            //   accountEmail: Text("61143215@g.cmru.ac.th"),
+            // ),
+            DrawerHeader(
+              margin: EdgeInsets.zero,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: NetworkImage(
+                    "https://cms-assets.tutsplus.com/uploads/users/41/posts/25951/image/material-design-background-2.jpg",
+                  ),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              child: Center(
+                child: Column(
+                  children: [
+                    CircleAvatar(
+                      backgroundImage: NetworkImage(
+                        "https://pbs.twimg.com/media/EcesYuNU0AAxiSy.jpg",
+                      ),
+                    ),
+                    Text("Home"),
+                  ],
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.people),
+              title: Text("Profile"),
+            )
+          ],
+        ),
+      ),
+      appBar: AppBar(
+        title: Text(pages[currentIndex].title),
+      ),
+      body: pages[currentIndex].page,
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentIndex,
+        onTap: onIndexChanged,
+        items: pages
+            .map(
+              (items) => BottomNavigationBarItem(
+                icon: items.icon, 
+                label: items.label,
+                ),
+            )
+            .toList(),
+      ),
+    );
+  }
+}
